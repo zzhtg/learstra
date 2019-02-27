ce8ee528b16f47cdb78bd9ad6fb0843346505a09
zzh <2559437829@qq.com>
2019-02-27 08:39:15 +0800

    rm some worthless file

--- install.sh
+++ install.sh
@@ -9,16 +9,10 @@ Server = https:\/\/mirrors.ustc.edu.cn\/archlinuxcn\/$arch/g' /etc/pacman.conf
 pacman -Syy && pacman -S archlinuxcn-keyring
 pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang make cmake python-pip shadowsocks-qt5
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm ~/.xprofile
-echo "export GTK_IM_MODULE=fcitx" >> ~/.xprofile
-echo "export QT_IM_MODULE=fcitx" >> ~/.xprofile
-echo "export XMODIFIERS=@im=fcitx" >> ~/.xprofile
-
-echo "[global]" >> ~/.pip/pip.conf
-echo "index-url = https://pypi.tuna.tsinghua.edu.cn/simple" >> ~/.pip/pip.conf
-echo "[install]" >> ~/.pip/pip.conf
-echo "trusted-host=mirrors.aliyun.com" >> ~/.pip/pip.conf
-
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mv .xprofile ~
+mv .pip ~
+mv .vim ~
 mv .vimrc ~
 vim .vimrc < EOF
 :PlugInstall
@@ -26,3 +20,6 @@ EOF
 
 cd ~/.vim/pluged/Youcompuleteme
 python install.py --all
+
+pacman -S tk
+pip install opencv-python matplotlib scipy
