c83175ef8eb83470dc590dae7bcb18150e0e1a3e
zzh <2559437829@qq.com>
2019-04-06 21:39:55 +0800

    remove /g

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@ pacman-mirrors -i -c China -m rank
 
 sed -i '$a [archlinuxcn]' /etc/pacman.conf
 sed -i '$a SigLevel = Optional TrustedOnly' /etc/pacman.conf
-sed -i '$a Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch/g' /etc/pacman.conf
+sed -i '$a Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf
 
 pacman -Syy && pacman -S archlinuxcn-keyring
 pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang make cmake python-pip shadowsocks-qt5
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mv .xprofile ~
-mv .pip ~
-mv .vim ~
-mv .vimrc ~
+cp .xprofile ~
+cp .pip ~
+cp .vim ~
+cp .vimrc ~
 vim .vimrc < EOF
 :PlugInstall
 EOF
