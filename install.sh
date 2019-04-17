1c88f72c31a3582165bd60e86942d435dab88bc0
zzh <2559437829@qq.com>
2019-04-18 01:12:56 +0800

    add lldb

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 pacman-mirrors -i -c China -m rank
+#Only Usting and ustc
 
 sed -i '$a [archlinuxcn]' /etc/pacman.conf
 sed -i '$a SigLevel = Optional TrustedOnly' /etc/pacman.conf
 sed -i '$a Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf
 
 pacman -Syy && pacman -S archlinuxcn-keyring
-pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang make cmake python-pip shadowsocks-qt5 --noconfirm
+pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang lldb make cmake python-pip shadowsocks-qt5 --noconfirm
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pwd
-mv .pip .vim .xprofile .vimrc ~
-ls -a ~
 
 vim .vimrc < EOF
 :PlugInstall
