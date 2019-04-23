b89505e8148aa1e62ed291e8222be39ad11466b7
zzh <2559437829@qq.com>
2019-04-24 03:18:21 +0800

    commant worthless code

--- install.sh
+++ install.sh
@@ -6,17 +6,16 @@ sed -i '$a SigLevel = Optional TrustedOnly' /etc/pacman.conf
 sed -i '$a Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf
 
 pacman -Syy && pacman -S archlinuxcn-keyring
-pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang lldb make cmake python-pip shadowsocks-qt5 --noconfirm
+pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang lldb make cmake python-pip shadowsocks-qt5 tk --noconfirm
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pwd
+ 
+# vim .vimrc < EOF
+# :PlugInstall
+# EOF
+# 
+# cd ~/.vim/pluged/Youcompuleteme
+# python install.py --all
 
-vim .vimrc < EOF
-:PlugInstall
-EOF
-
-cd ~/.vim/pluged/Youcompuleteme
-python install.py --all
-
-pacman -S tk
 pip install opencv-python matplotlib scipy pyserial
