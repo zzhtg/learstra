25a913ab62ede4e6c549af7fd2c3ca217d181afe
zonghao <zhaozhan1215@outlook.com>
2019-08-25 22:44:12 +0800

    replace ubuntu /etc/apt/source.list by sudo bash -c

--- linuxconf/manjaro_conf.sh
+++ linuxconf/manjaro_conf.sh
@@ -1,12 +1,14 @@
-pacman-mirrors -i -c China -m rank
+sudo pacman-mirrors -i -c China -m rank
 #Only Usting and ustc
 
-sed -i '$a [archlinuxcn]' /etc/pacman.conf
-sed -i '$a SigLevel = Optional TrustedOnly' /etc/pacman.conf
-sed -i '$a Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' /etc/pacman.conf
+sudo bash -c "echo '
+[archlinuxcn]
+SigLevel = Optional TrustedOnly
+Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
+' >> /etc/pacman.conf"
 
-pacman -Syy && pacman -S archlinuxcn-keyring
-pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang lldb make cmake python-pip shadowsocks-qt5 tk --noconfirm
+sudo pacman -Syy && pacman -S archlinuxcn-keyring
+sudo pacman -S zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang lldb make cmake python-pip shadowsocks-qt5 tk --noconfirm
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- linuxconf/ubuntu_conf.sh
+++ linuxconf/ubuntu_conf.sh
@@ -1,9 +1,25 @@
-sudo mv /etc/apt/source.list /etc/apt/source.list.bak
-sudo cp source.list /etc/apt/
+sudo cp /etc/apt/source.list /etc/apt/source.list.bak
+
+sudo bash -c "echo '
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+' > /etc/apt/source.list"
+
 sudo apt update && sudo apt upgrade
 sudo apt dist-upgrade
 sudo apt autoremove
-sudo apt install zsh git cmake make vim python3
+sudo apt install zsh git cmake make vim python3-pip
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
