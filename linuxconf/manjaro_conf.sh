b2986d0e0aedcb01b93ae0899e451758672491a9
zhaozhan <zhaozhan1215@outlook.com>
2020-03-14 01:38:24 +0800

    update oh-my-zsh install and archlinuxcn

--- linuxconf/manjaro_conf.sh
+++ linuxconf/manjaro_conf.sh
@@ -4,19 +4,19 @@ sudo pacman-mirrors -i -c China -m rank
 sudo bash -c "echo '
 [archlinuxcn]
 SigLevel = Optional TrustedOnly
-Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch
+Server = https://mirrors.163.com/archlinux-cn/$arch
 ' >> /etc/pacman.conf"
 
-sudo pacman -Syy && pacman -S archlinuxcn-keyring
-sudo pacman -S curl zsh fcitx-im fcitx fcitx-sogoupinyin fcitx-configtool git vim clang lldb make cmake python-pip shadowsocks-qt5 tk --noconfirm
+sudo pacman -Syy && sudo pacman -Sy archlinuxcn-keyring --noconfirm
+sudo pacman -S curl code zshgit vim clang lldb make cmake python-pip --noconfirm
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vim/colors/gruvbox.vim --create-dirs https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
 cp .vimrc ~
 
-python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
-python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+sudo python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
+sudo python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
 echo "Open your vim and run :PlugInstall"
 echo "Then, you need run $:python3 ~/.vim/plugged/YouCompleteMe/install.py --all"
